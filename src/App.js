974e396f2abddbee2c9aaf78f436a0fcb7b2dcf1
geethasu <nxtwave@ip-10-2-0-51.ap-south-1.compute.internal>
2024-07-06 05:59:42 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Switch, Route} from 'react-router-dom'
+
+import Home from './components/Home'
+import About from './components/About'
+import StateSpecificDetails from './components/StateSpecificDetails'
+import Vaccination from './components/Vaccination'
+import NotFound from './components/NotFound'
+import Footer from './components/Footer'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/state/:stateCode" component={StateSpecificDetails} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/vaccination" component={Vaccination} />
+    <Route component={NotFound} />
+    <Route component={Footer} />
+  </Switch>
+)
+
+export default App
